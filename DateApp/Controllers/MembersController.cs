7512f682fbb309cb47bf58cf40279384b611fb7c
nguyenhuuanh21 <nguyenhuuanh.user@gmail.com>
2026-06-22 11:29:07 +0700

    second commit

---
+++ DateApp/Controllers/MembersController.cs
@@ -0,0 +1,30 @@
+﻿using DateApp.Data;
+using DateApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetMembers()
+        {
+            var members= await context.AppUsers.ToListAsync();
+            return Ok(members);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember([FromRoute]string id)
+        {
+            var member =await context.AppUsers.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return Ok(member);
+        }
+    }
+}
---
+++ DateApp/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using DateApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateApp.Data
+{
+    public class AppDbContext(DbContextOptions options) :DbContext(options)
+    {
+        public DbSet<AppUser>AppUsers { get; set; }
+    }
+}
